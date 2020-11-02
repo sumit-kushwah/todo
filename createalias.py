d00d76081eaf4242ef105d4d376dde37ed0e6600
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-11-02 18:25:40 +0530

    minor fixes

--- createalias.py
+++ createalias.py
@@ -7,9 +7,9 @@ stream = os.popen('cp todo.py temp.py')
 stream = os.popen('chmod +x temp.py')
 stream = os.popen('mv temp.py todo') # command as todo
 cwd = os.getcwd()
-print(bcolors.WARNING + f'alias todo=\'{cwd}/todo\'' + bcolors.ENDC)
+print(bcolors.WARNING + 'add below line to your shell configuration file (i.e .zshrc or .bashrc).\n' + bcolors.ENDC)
+print(bcolors.BOLD + bcolors.OKBLUE + f'\talias todo=\'{cwd}/todo\' \n' + bcolors.ENDC)
 
-# stream = os.popen('cd ..')
 
 
 
--- todo.py
+++ todo.py
@@ -4,13 +4,13 @@ import argparse
 import sys
 from manager import *
 
-parser = argparse.ArgumentParser(description='A todo command line application')
+parser = argparse.ArgumentParser(description='A smart command line todo application.')
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-a", "--add", help="add task", nargs='+')
 group.add_argument("-l", "--list", help="list tasks", action="store_true")
 group.add_argument("-due", "--overdue", help="list  overdue tasks", action="store_true")
-group.add_argument("-dp", "--deleteproject", help="delete a project")
-group.add_argument("-lp", "--listprojects", help="list projects", action="store_true")
+group.add_argument("--deleteproject", help="delete a project")
+group.add_argument("--projects", help="list projects", action="store_true")
 group.add_argument("-u", "--upcoming", help="list upcoming tasks", action="store_true")
 parser.add_argument("-p", "--project", help="specify project name")
 args = parser.parse_args()
@@ -18,17 +18,18 @@ args = parser.parse_args()
 if (len(sys.argv) == 1):
     parser.print_help()
     sys.exit(1)
-else:
-    manager = Manager()
-    if args.add:
-        manager.addTasks(args.add, args.project)
-    if args.list:
-        manager.listTasks(args.project)
-    if args.overdue:
-        manager.listDueTasks(args.project)
-    if args.upcoming:
-        manager.listUpcomingTasks(args.project)
-    if args.listprojects:
-        manager.listProjects()
-    if args.deleteproject:
-        manager.deleteProject(args.project)
+
+manager = Manager()
+
+if args.add:
+    manager.addTasks(args.add, args.project)
+if args.list:
+    manager.listTasks(args.project)
+if args.overdue:
+    manager.listDueTasks(args.project)
+if args.upcoming:
+    manager.listUpcomingTasks(args.project)
+if args.projects:
+    manager.listProjects()
+if args.deleteproject:
+    manager.deleteProject(args.project)
