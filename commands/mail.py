1db9cdd9e03b8d3afb69e9b664a110618d5c8d32
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-02 09:01:01 +0530

    mail command added

--- commands/mail.py
+++ commands/mail.py
@@ -1,13 +1,19 @@
 import yagmail
 from commands import listit
-from config import mailusername, mailpassword
+from config import mailusername
 
-yag = yagmail.SMTP(mailusername, mailpassword)
-contents = [
-    "This is the body, and here is just text http://somedomain/image.png",
-    "You can find an audio file attached.", '/local/path/to/song.mp3'
-]
+yag = yagmail.SMTP(mailusername)
+
+def htmlText(tasks, title):
+    html = f"<h2>{title}</h2>"
+    html += "<ul>"
+    for task in tasks:
+        html += f"<li style=\"font-size: 20px;\">  {task.description} </li>"
+    html += "</ul>"
+    return html
+
 
 def send(args):
     data = listit.getmaillist(args)
-    yag.send(args.tos, data["subject"], contents)
+    htmltext = htmlText(data["tasks"], data["subject"])
+    yag.send(args.tos, data["subject"], htmltext)
---
+++ config.py
@@ -0,0 +1,13 @@
+from inquirer.themes import *
+
+# firebase settings
+firebaseSDKFile = "/Users/sumitkushwah/Desktop/CWP/todo/configurations/firebase.json"
+databaseUrl = "https://todo-8370f.firebaseio.com/"
+
+# general settings
+dbfilepath = "./tasks.json"
+theme = GreenPassion() # greenpassion or None (for default theme)
+
+# mail settings
+mailusername = "sumitkushwah1729"
+
