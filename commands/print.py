c65b390701f8ab51e8d9819d984d7e8ac0d8c438
sumit-kushwah <sumitkushwah1729@gmail.com>
2020-12-03 17:49:07 +0530

    pdf module changed

--- commands/print.py
+++ commands/print.py
@@ -1,12 +1,13 @@
-import pydf
+import pdfkit
 from utils import htmlgenerator
 from commands import listit
 
 def print(args):
     data = listit.getTasksAndTitle(args)
     htmltext = htmlgenerator.generate(data["tasks"], data["title"])
-    pdf = pydf.generate_pdf(htmltext)
-    filename = args.filename or "~/Downloads/todos.pdf"
-    with open(filename, 'wb') as f:
-        f.write(pdf)
-    f.close()
+    # pdf = pydf.generate_pdf(htmltext)
+    filename = ("Users/sumitkushwah/Downloads" + args.filename) or "/Users/sumitkushwah/Downloads/todos.pdf"
+    try:
+        pdfkit.from_string(htmltext, filename)
+    except:
+        print("Some failure occurred while generating pdf.")
